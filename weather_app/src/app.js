cae189ac53dcf0b7f431487c79aa3f365596f374
SvetoslavSlavov <svetoslav_slavov@icloud.com>
2017-08-09 15:21:36 +0300

    First

---
+++ weather_app/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactNative from 'react-native';
+import { Provider } from 'react-redux';
+
+const App = () => {
+    return (
+        <Provider />    
+    );
+};
